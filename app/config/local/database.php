75b076657938051c6bcb5d05e6a3c54c10783ba6
astrsk-hori <horie_go_gn@cyberagent.co.jp>
2014-09-30 13:53:40 +0900

    local環境設定とusersテーブルを追加した

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -23,9 +23,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'homestead',
-			'username'  => 'homestead',
-			'password'  => 'secret',
+			'database'  => 'sampledb',
+			'username'  => 'root',
+			'password'  => 'rootpasswd',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
