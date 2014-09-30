57a097aae78d7470368da46716047b9d175cef66
astrsk-hori <horie_go_gn@cyberagent.co.jp>
2014-09-30 17:53:06 +0900

    usersの初期データ登録

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -2,16 +2,33 @@
 
 class DatabaseSeeder extends Seeder {
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		Eloquent::unguard();
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+  */
+  public function run()
+  {
+    Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
-	}
+    $this->call('UserTableSeeder');
 
+    $this->command->info('User table seeded!');
+  }
+
+}
+
+class UserTableSeeder extends Seeder {
+
+  public function run()
+  {
+    DB::table('users')->truncate();
+
+    User::create(
+      array(
+        'name' => 'hoge',
+        'email' => 'foo@bar.com'
+      )
+    );
+  }
 }
